6cae49ec0015c0e7fa437e2d8b1a7a4b955cb262
ImoutoChan <kirirrin@gmail.com>
2017-05-15 01:16:57 +0300

    date format and version update

--- ShikiDates.user.js
+++ ShikiDates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shiki History Dates
 // @namespace    http://shikimori.org/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Dates in history on shikimori.org
 // @author       ImoutoChan
 // @match        https://shikimori.org/*
